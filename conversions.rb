1eb2df7eecbeea2f20bb2caf6f4026609ef5bf01
Keith Filkins <kafilkins@me.com>
2020-11-04 20:34:15 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -2,3 +2,9 @@ GRAMS_PER_OUNCE = 28.3495
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
+
+GRAMS_PER_OUNCE = 36
+
+def ounces_to_grams(ounces)
+  ounces_to_f * GRAMS_PER_OUNCE
+end
